ca7733c6b6cb582123156515c06b0d6e02c8f183
mirza@Regulus <exquisitemirza@student.ub.ac.id>
2023-05-11 06:24:12 +0700

    ``docs``: Mention firebase credential & Migrate CLI on README setup

--- startup.sh
+++ startup.sh
@@ -3,7 +3,6 @@ set -e
 
 source .env
 
-# Run your SQL migration script using the MariaDB client
 echo "Running SQL migrations..."
 mysql -h $DB_HOST -u $DB_USERNAME -p$DB_PASSWORD $DB_DATABASE < migration/000001_create_contests_table.up.sql
 mysql -h $DB_HOST -u $DB_USERNAME -p$DB_PASSWORD $DB_DATABASE < migration/000002_create_participants_table.up.sql
