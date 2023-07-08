e619f538af76a8bcc63e66d5ba0dd74f6ec09e3d
mirza@Regulus <exquisitemirza@student.ub.ac.id>
2023-07-08 11:04:00 +0700

    ``feat``: Add contest's clean struct for better & faster response footprint

--- domain/contest.go
+++ domain/contest.go
@@ -6,14 +6,19 @@ import (
 )
 
 type Contest struct {
-	ID        int       `json:"id"`
-	Name      string    `json:"name"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
+	ID        int
+	Name      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
 type StoreContest struct {
 	Name string `json:"name" binding:"required"`
+}
+
+type CleanContest struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
 func (c *StoreContest) Validate() error {
