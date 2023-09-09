5b1e29700dbb9413d4f7ee146a6222452636a45d
mirza@Regulus <exquisitemirza@student.ub.ac.id>
2023-09-09 14:05:34 +0700

    ``feat(Google-OAuth)``: Implement usecase layer along with its OAuth

--- api/oauth/repository/oauth.go
+++ api/oauth/repository/oauth.go
@@ -56,6 +56,7 @@ func (r *OAuthRepository) UpdateProviderID(id int, providerID string, ctx contex
 
 	return nil
 }
+
 func (r *OAuthRepository) BeginTx() (*sqlx.Tx, error) {
 	return r.mysql.Beginx()
 }
---
+++ domain/claim.go
@@ -0,0 +1,14 @@
+package domain
+
+import "github.com/golang-jwt/jwt/v4"
+
+type GoogleClaims struct {
+	Id            string `json:"id"`
+	Email         string `json:"email"`
+	VerifiedEmail bool   `json:"verified_email"`
+	Name          string `json:"name"`
+	GivenName     string `json:"given_name"`
+	Picture       string `json:"picture"`
+	Locale        string `json:"locale"`
+	jwt.StandardClaims
+}
