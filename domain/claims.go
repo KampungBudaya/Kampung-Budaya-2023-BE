a5b3224ad9080e4c2cd545f2b5be67cc342b5838
mirza@Regulus <exquisitemirza@student.ub.ac.id>
2023-09-09 17:34:25 +0700

    ``feat(Roles)``: Implement JWT Authentication that also includes roles

--- domain/claims.go
+++ domain/claims.go
@@ -2,6 +2,12 @@ package domain
 
 import "github.com/golang-jwt/jwt/v4"
 
+type AuthClaims struct {
+	ID    int    `json:"id"`
+	Roles string `json:"roles"`
+	jwt.RegisteredClaims
+}
+
 type GoogleClaims struct {
 	ID            string `json:"id"`
 	Email         string `json:"email"`
